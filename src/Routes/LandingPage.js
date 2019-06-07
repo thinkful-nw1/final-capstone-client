0bf1f0ceab2fa41b8f4793647cdca1765dcef31c
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-06-06 20:57:03 -0400

    created landing page

---
+++ src/Routes/LandingPage.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class LandingPage extends React.Component {
+    render() {
+        return (
+            <main role="main">
+                <header role="banner">
+                    <h1>[Trip Planning App]</h1>
+                    <h2>[All your travel plans in one place]</h2>
+                </header>
+            <section>
+                [Plan your trip with one click! Enter your destination and get flights, sights, and eats to add to your travel itinerary.] 
+            </section>
+            <section class='card-signup-form'>
+                <header>
+                    <h3>Sign Up Now</h3>
+                </header>
+                <form class='signup-form'>
+                    <div class="form-field">
+                        <label for="username" >Username</label>
+                        <input type="text" name='username' id='username' />
+                    </div>
+                    <div class="form-field">
+                        <label for="email">Email</label>
+                        <input type="email" name='email'/>
+                    </div>
+                    <div class="form-field">
+                        <label for="password">Password</label>
+                        <input type="password" name='password' id='password' />
+                    </div>
+                <button type='submit'>Join</button>
+                </form>
+            </section>
+            </main>
+        )
+    }
+}
