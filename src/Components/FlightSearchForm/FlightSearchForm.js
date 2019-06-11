aeca24b94eb8ebfcc06b53659675a1331c152e44
Jordan Epps <jordanepps@gmail.com>
2019-06-11 17:15:42 -0400

    Add airport data for user to search

--- src/Components/FlightSearchForm/FlightSearchForm.js
+++ src/Components/FlightSearchForm/FlightSearchForm.js
@@ -1,22 +1,139 @@
-import React from 'react'
-import './FlightSearchForm.css'
+import React from 'react';
+import airports from '../../services/airports';
+import './FlightSearchForm.css';
 
-export default class FlightSearchForm extends React.Component{
-    render() {
-        return(
-          <div class="flight-form-card">
-            <form class="flight-searchform">
-              <input type="text" name="flight-origin"
-                    placeholder="From"/>
-              <input type="text" name="flight-destination"
-                    placeholder="To"/>
-              <input type="text" name="flight-depart"
-                    placeholder="Today"/>
-              <input type="text" name="flight-return"
-                    placeholder="Tomorrow"/>
-              <button type="submit">Search</button>
-            </form>     
-          </div> 
-        )
+export default class FlightSearchForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toInputValue: '',
+      fromInputValue: '',
+      showToResults: false,
+      showFromResults: false,
+      airportSearch: []
+    };
+  }
+
+  handleToInput = (e, toOrFrom) => {
+    const inputValue = e.target.value;
+    this.setState({
+      toInputValue: inputValue,
+      showToResults: true,
+      showFromResults: false
+    });
+    this.handleAirportLookup(inputValue, toOrFrom);
+  };
+
+  handleFromInput = e => {
+    const inputValue = e.target.value;
+    this.setState({
+      fromInputValue: inputValue,
+      showFromResults: true,
+      showToResults: false
+    });
+    this.handleAirportLookup(inputValue);
+  };
+
+  handleAirportLookup = (value, toOrFrom) => {
+    if (value.length >= 2) {
+      const airportSearch = airports.filter(airport =>
+        this.findPossibleAirports(airport, value)
+      );
+      this.setState({ airportSearch });
+    } else {
+      this.setState({ airportSearch: [] });
     }
+  };
+
+  findPossibleAirports = (airport, value) => {
+    if (
+      airport.code.toLowerCase().includes(value) ||
+      airport.city.toLowerCase().includes(value) ||
+      airport.state.toLowerCase().includes(value) ||
+      airport.name.toLowerCase().includes(value)
+    )
+      return airport;
+  };
+
+  handleAirportClick = i => {
+    const airport = this.state.airportSearch[i];
+    if (this.state.showFromResults) {
+      this.setState({ fromInputValue: airport.code, showFromResults: false });
+      this.from.value = airport.code;
+    } else {
+      this.setState({ toInputValue: airport.code, showToResults: false });
+      this.to.value = airport.code;
+    }
+  };
+
+  handleKeyPress = (e, i) => {
+    if (e.key === 'Enter') {
+      this.handleAirportClick(i);
+    }
+  };
+
+  renderList = (airport, i) => {
+    return (
+      <li
+        key={i}
+        className="airport"
+        role="button"
+        tabIndex={0}
+        onClick={() => this.handleAirportClick(i)}
+        onKeyPress={e => this.handleKeyPress(e, i)}
+      >
+        <span>
+          {airport.name} - {airport.code}
+        </span>
+        <br />
+        <span>
+          {airport.city}, {airport.state}
+        </span>
+      </li>
+    );
+  };
+
+  render() {
+    const { showToResults, showFromResults, airportSearch } = this.state;
+    return (
+      <div className="flight-form-card">
+        <form className="flight-search-form">
+          <div className="input-container">
+            <input
+              type="text"
+              name="flight-to"
+              placeholder="From"
+              onChange={e => this.handleFromInput(e, 'from')}
+              ref={input => (this.from = input)}
+            />
+            <ul className={showFromResults ? 'search-box' : 'hide-box'}>
+              {airportSearch.map(this.renderList)}
+            </ul>
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              name="flight-from"
+              placeholder="To"
+              onChange={e => this.handleToInput(e)}
+              ref={input => (this.to = input)}
+            />
+            <ul className={showToResults ? 'search-box' : 'hide-box'}>
+              {airportSearch.map(this.renderList)}
+            </ul>
+          </div>
+
+          <input type="date" name="flight-depart" />
+          <input type="date" name="flight-return" />
+          <input
+            type="number"
+            name="passengers"
+            min="1"
+            placeholder="Passengers"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
 }
--- src/Routes/SearchPage.js
+++ src/Routes/SearchPage.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import FlightSearchForm from '../Components/FlightSearchForm/FlightSearchForm'
-import FeedCard from '../Components/FeedCard'
+import React from 'react';
+import FlightSearchForm from '../Components/FlightSearchForm/FlightSearchForm';
+import FeedCard from '../Components/FeedCard';
 
-export default class SearchPage extends React.Component{
-    render() {
-        return (
-            <main role="main">
-                <section class="form-section">
-                    <FlightSearchForm />
-                </section>
-                <section class="feed-section">
-                    <FeedCard />
-                </section>
-            </main>
-        )
-    }
+export default class SearchPage extends React.Component {
+  render() {
+    return (
+      <main role="main">
+        <section className="form-section">
+          <FlightSearchForm />
+        </section>
+        <section className="feed-section">
+          <FeedCard />
+        </section>
+      </main>
+    );
+  }
 }
